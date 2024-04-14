5f212e23723eb3497f9933837e31479b56a8debd
Omar Faruk <omarfaruk65142@gmail.com>
2024-04-14 12:11:53 +0600

    Try to understand local module

---
+++ module/local-module/index.js
@@ -0,0 +1,5 @@
+//import here from student.js
+const student = require('./student')
+console.log(student.getName())
+console.log(student.getAge())
+console.log(student.cgpa)
---
+++ module/local-module/student.js
@@ -0,0 +1,15 @@
+const getName = () => {
+    return "Md. Omar Faruk"
+}
+const getAge = () => {
+    return 25
+}
+const cgpa = 3.50;
+// exports.getName = getName
+// exports.getAge = getAge
+// exports.cgpa = cgpa;
+module.exports = {
+    getAge,
+    getName,
+    cgpa
+}
