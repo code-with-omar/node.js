40632a107e4503c9d18e7cabadce5e3342522feb
Omar Faruk <omarfaruk65142@gmail.com>
2024-04-14 12:25:54 +0600

    local module

--- module/local-module/index.js
+++ module/local-module/index.js
@@ -3,3 +3,8 @@ const student = require('./student')
 console.log(student.getName())
 console.log(student.getAge())
 console.log(student.cgpa)
+
+
+//find only getName
+const { getName } = require('./student')
+console.log("Name :", getName())
